bd4cbf2056379a68d068505be97702c25fbbae47
elize21 <106829163+elize21@users.noreply.github.com>
2022-06-08 08:11:35 +0300

    Create options.js

---
+++ plugins/options.js
@@ -0,0 +1,13 @@
+{
+    "botName": "DREADEDBOT",
+    "ownerName": "teslasmastermind",
+    "BarBarKey": "YOUR_APIKEY",
+    "ownerNumbers": "254784841589@s.whatsapp.net",
+    "languages": "ind",
+    "botPrefix": ".",
+    "memberLimitss": "15",
+    "userDefaultLimit": "20",
+    "TobzApi": "BotWeA",
+    "ZeksApi": "apivinz",
+    "TechApi": "YOUR_APIKEY"
+}
