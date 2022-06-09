ed285fde19f6cd64b83c43546ee4c204a23c2e4d
elize21 <106829163+elize21@users.noreply.github.com>
2022-06-09 15:02:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 @@ -1,4 +1,4 @@
-global.owner = ['6281515860089'] // Put your number here
-global.owner = ['6281515860089','6281334177340'] // Put your number here
+global.owner = ['+254784841589'] // Put your number here
+global.owner = ['+254784841589','6281334177340'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -11,7 +11,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b'
+  'https://api.xteam.xyz': '9dbfefbd20a1b284'
 }
 // Sticker WM
 global.packname = 'I hope you\'re fine'
