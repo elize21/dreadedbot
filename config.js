c3dda6d1ad75da2c6a27fc52e1e8addc91ef9f05
elize21 <106829163+elize21@users.noreply.github.com>
2022-06-09 05:30:15 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+
+@@ -1,4 +1,4 @@
+global.owner = ['6281515860089'] // Put your number here
+global.owner = ['6281515860089','6281334177340'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b'
+}
+// Sticker WM
+global.packname = 'I hope you\'re fine'
+global.author = 'Nurutomo'
+global.multiplier = 69 // The higher, The harder levelup
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
